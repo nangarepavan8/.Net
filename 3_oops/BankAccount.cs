ba02733f97fa522ff3755f363ca97d90803e6a57
PAVAN SAMBHAJI NANGARE-PATIL <62687233+nangarepavan8@users.noreply.github.com>
2022-06-10 13:23:20 +0530

    Bank Money Deposit and Transaction  Program
    Transaction of Money that is deposit from bank account and the withdraw some money . related to that program as well as throw some exceptions in that to remove errors and stop to avoid compilation in case of any problem occur.

---
+++ 3_oops/BankAccount.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_oops
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+
+        public string Owner { get; set; }
+
+        public decimal Balance {
+            get
+            {
+                decimal balance = 0;
+                   
+                foreach (var item in allTransaction)
+                {
+                    balance = balance + item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        // take user input 
+        public decimal amount { get; set; }
+
+
+
+        private static int AccountNumberSeed = 1;
+
+        private List<Transaction> allTransaction = new List<Transaction>();
+
+
+        // name balance and account number
+        public BankAccount(string name , decimal initialBalance)
+        {
+            this.Owner = name;
+
+            // this.Balance = initialBalance;
+
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance"); 
+            
+            this.Number = AccountNumberSeed.ToString();
+            AccountNumberSeed++;
+
+
+        }
+
+
+        public void MakeDeposit(decimal  amount, DateTime date,  string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount the deposite must be positive");
+            }
+
+            var deposit = new Transaction(amount, date, note);
+            allTransaction.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount , DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount the deposite must be positive");
+                // throw new InvalidOperationException("Not sufficient funds to Withdrawal ");
+            }
+
+            if( Balance - amount <= 0 )
+            {
+                throw new InvalidOperationException("Not sufficient funds to Withdrawal ");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransaction.Add(withdrawal);
+            allTransaction.OrderByDescending();
+
+
+       public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+
+            //HEADER
+            report.AppendLine("Date \t Amount \t Note");
+
+            foreach(var item in allTransaction)
+            {
+                //ROWS
+                report.AppendLine($"{item.Date} \t {item.Amount} \t {item.Notes}" );
+            }
+
+            return report.ToString();
+
+        }
+    }
+}
---
+++ 3_oops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_oops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var account = new BankAccount("Pavan", 5000);
+
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} and  Account Balance is {account.Balance}");
+
+            account.MakeDeposit(500, DateTime.Now, "personal use") ;
+           // account.MakeDeposit( DateTime.Now, "personal use");
+            Console.WriteLine($"You are deposited Amount {account.amount} aand remaining balance is  {account.Balance} .");
+
+            account.MakeWithdrawal(1000, DateTime.Now, "Bike");
+            Console.WriteLine($"You are Withdrawal {account.Balance} Amount");
+
+            Console.WriteLine(account.GetAccountHistory());
+
+
+
+            account.MakeDeposit(-500, DateTime.Now, "personal use");
+        }
+    }
+}
